6ab9ef3d4d6e1d633f8627c3fa69b4aec81e7e1b
chocoscoding <chocoscoding@gmail.com>
2022-02-17 00:11:18 +0100

    create todos done

--- src/contollers/Allrequests.ts
+++ src/contollers/Allrequests.ts
@@ -12,12 +12,22 @@ interface postT {
   limit: number;
   page: number;
   TodoTitle: any;
-  TodoId: number;
   userId: number;
   TodoStatus: boolean;
   resultType: any;
+  id: number;
   msg: string;
   code: number;
+}
+
+function bodycheck(datachecked: any) {
+  const { TodoStatus, TodoTitle, userId, resultType, id }: postT = datachecked;
+
+  if (!userId || !TodoStatus === undefined || !TodoTitle || !id) {
+    return true;
+  } else {
+    return false;
+  }
 }
 var auJson = require("../../apidata/allUsers.json");
 function getPageArrangement(queryT: queryTi) {
@@ -46,7 +56,6 @@ const getComments = (req: Request, res: Response) => {
 };
 const getTodos = (req: Request, res: Response) => {
   const reqQueryLen: number = Object.keys(req.query).length;
-  console.log(req.body);
 
   if (reqQueryLen < 1) {
     try {
@@ -69,68 +78,82 @@ const getTodos = (req: Request, res: Response) => {
       .json({ result: processingdata, amount: limit, page });
   }
 };
-const postTodos = (req: Request, res: Response) => {
-  const data: JSON = atJson;
-  const data2: any = Object.values(atJson);
-  const {
-    limit,
-    page,
-    TodoId,
-    TodoStatus,
-    TodoTitle,
-    userId,
-    resultType,
-  }: postT = req.body;
-
-  if (
-    !userId ||
-    !TodoId ||
-    !TodoStatus === undefined ||
-    !TodoTitle ||
-    !resultType === undefined ||
-    resultType !== "mutated"
-  ) {
-    if (resultType !== "mutated") {
-      const arrtcheck = [
-        { type: userId, name: "userId" },
-        { type: TodoId, name: "TodoId" },
-        { type: TodoStatus, name: "TodoStatus" },
-        { type: TodoTitle, name: "TodoTitle" },
-        { type: resultType, name: "resultType" },
-      ];
-      let arrtReceive: any = [];
-      arrtcheck.forEach((element) => {
-        if (element.name === "resultType") {
-          if (element.type === undefined || element.type === "mutated") {
-            console.log("right");
-          } else {
-            // console.log('wrong');
-            arrtReceive.push(element.name);
-          }
-        } else if (!element.type) {
-          arrtReceive.push(element.name);
-        }
-      });
-      const msg = `Provide the valid data for ${arrtReceive.toString()}`;
-      return res.status(403).json({ msg });
-    }
-    // return res.status(403).json({msg: 'error'})
-  } else {
+const createTodos = (req: Request, res: Response) => {
+  const resarr = new Array(...atJson);
+  const { limit, page, TodoStatus, TodoTitle, userId, resultType, id }: postT =
+    req.body;
+  if (!bodycheck(req.body)) {
     const resType: undefined | string = resultType || "normal";
-    if (resType === "normal") {
+    if (resType === "normal" || resType === undefined) {
       return res.status(200).json({
-        id: 1,
+        userId,
+        id,
+        title: TodoTitle,
+        completed: TodoStatus,
       });
     } else {
-      res.json(atJson);
+      const limit_dec: number = ~~(limit || 20); //the limit per page declaration
+      const page_dec: number = ~~(page || 1); //the page number declaration
+
+      if (limit >= 1) {
+        let rawdata = [
+          ...atJson,
+          {
+            userId,
+            id,
+            title: TodoTitle,
+            completed: TodoStatus,
+          },
+        ];
+        const pageArr = getPageArrangement({
+          limit: limit_dec,
+          page: page_dec,
+        });
+        const processingdata = rawdata.slice(
+          pageArr.sliceStart,
+          pageArr.sliceLimit
+        );
+        return res
+          .status(200)
+          .json({ result: processingdata, amount: limit_dec, page: page_dec });
+      } else {
+        return res.status(200).json([
+          ...atJson,
+          {
+            userId,
+            id,
+            title: TodoTitle,
+            completed: TodoStatus,
+          },
+        ]);
+      }
     }
+  } else {
+    const arrtcheck = [
+      { type: userId, name: "userId" },
+      { type: id, name: id },
+      { type: TodoTitle, name: "TodoTitle" },
+      { type: resultType, name: "resultType" },
+    ];
+    let arrtReceive: any = [];
+    arrtcheck.forEach((element) => {
+      if (element.name === "resultType") {
+        if (element.type === undefined || element.type === "mutated") {
+          console.log("right");
+        } else {
+          // console.log('wrong');
+          arrtReceive.push(element.name);
+        }
+      } else if (!element.type) {
+        arrtReceive.push(element.name);
+      }
+    });
+    const msg = `Provide the valid data for ${arrtReceive.toString()}`;
+    return res.status(403).json({ msg });
+    // return res.status(403).json({msg: 'error'})
   }
 };
 const deleteTodos = (req: Request, res: Response) => {
-  const data: JSON = atJson;
-  res.json(atJson);
-};
-const createTodos = (req: Request, res: Response) => {
   const data: JSON = atJson;
   res.json(atJson);
 };
@@ -144,7 +167,6 @@ export {
   getUsers,
   getComments,
   getTodos,
-  postTodos,
   editTodos,
   createTodos,
   deleteTodos,
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,11 +1,24 @@
-import express  from "express";
+import express from "express";
 const router = express.Router();
 
-import { getPosts, getUsers, getComments, getTodos, postTodos, editTodos, createTodos, deleteTodos } from '../contollers/Allrequests';
+import {
+  getPosts,
+  getUsers,
+  getComments,
+  getTodos,
+  editTodos,
+  createTodos,
+  deleteTodos,
+} from "../contollers/Allrequests";
 
-router.route('/posts').get(getPosts)
-router.route('/users').get(getUsers)
-router.route('/comments').get(getComments)
-router.route('/todos').get(getTodos).post(postTodos).patch(editTodos).delete(deleteTodos)
+router.route("/posts").get(getPosts);
+router.route("/users").get(getUsers);
+router.route("/comments").get(getComments);
+router
+  .route("/todos")
+  .get(getTodos)
+  .post(createTodos)
+  .patch(editTodos)
+  .delete(deleteTodos);
 
-export { router}
+export { router };
