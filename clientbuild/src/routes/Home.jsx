cdda01cb7ef137d98e553edadd3f52388a2ca80b
Chocos coding <chocoscoding@gmail.com>
2022-03-02 17:18:23 +0100

    jhj

---
+++ clientbuild/src/routes/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import index from "../styles/index.module.css";
+
+const Home = () => {
+  return (
+    <div className={index.gradient_bg_services}>
+      Home
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+      <div className={index.gradient_bg_services}></div>
+    </div>
+  );
+};
+
+export default Home;
